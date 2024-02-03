df5785cb28be5987b424682f0d10d37bafaa6a25
Apuroop-babu <122634154+Apuroop-babu@users.noreply.github.com>
2024-02-03 22:12:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,23 +89,23 @@ def main():
          else:
              st.error("Invalid username or password. Please try again.")
 
-    st.set_page_config("Chat PDF")
-    st.header("Upload your pdf files and start asking questions")
+     st.set_page_config("Chat PDF")
+     st.header("Upload your pdf files and start asking questions")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+     user_question = st.text_input("Ask a Question from the PDF Files")
 
-    if user_question:
-        user_input(user_question)
+     if user_question:
+         user_input(user_question)
 
-    with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+     with st.sidebar:
+         st.title("Menu:")
+         pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+         if st.button("Submit & Process"):
+             with st.spinner("Processing..."):
+                 raw_text = get_pdf_text(pdf_docs)
+                 text_chunks = get_text_chunks(raw_text)
+                 get_vector_store(text_chunks)
+                 st.success("Done")
 
 if __name__ == "__main__":
     main()
