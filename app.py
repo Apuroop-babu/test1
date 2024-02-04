5f9bab0318284b8b15da641d75b680998a4a0825
Apuroop-babu <122634154+Apuroop-babu@users.noreply.github.com>
2024-02-04 13:50:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,31 +67,14 @@ def user_input(user_question):
 
     print(response)
     st.write("Reply: ", response["output_text"])
-def authenticate(username, password):
-    # Replace this function with your actual authentication logic
-    # For example, you might check credentials against a database
-    # In this example, we use a simple hardcoded check
-    return username == "demo" and password == "password"
+
     
 def main():
      st.set_page_config("Chat PDF")
      st.header("Upload your pdf files and start asking questions")
 
      user_question = st.text_input("Ask a Question from the PDF Files")
-     st.title("Login Page")
-
-    # Input fields for username and password
-     username = st.text_input("Username:")
-     password = st.text_input("Password:", type="password")
-
-    # Check if the login button is clicked
-     if st.button("Login"):
-        # Perform authentication
-         if authenticate(username, password):
-             st.success("Login successful!")
-            # Add your main application code here
-         else:
-             st.error("Invalid username or password. Please try again.")
+    
 
      if user_question:
          user_input(user_question)
